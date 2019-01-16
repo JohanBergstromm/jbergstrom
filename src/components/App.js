b7e0927c68b65e4c1e478fdddf5de67d5acebb9a
Johan Bergström <johanbergstrom@Johans-iMac.local>
2019-01-16 10:24:58 +0100

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import Menu from './menu';
 import Footer from './footer';
+import Header from './header';
+import Form from './form';
+import Weather from './weather';
 
-let test = "Walla";
+const test = 'Walla';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      	<Menu test={test}/>
-        <h1>My React App!</h1>
-      	<Footer />
-      </div>
-    );
-  }
+	state = {
+		city: undefined
+	}
+
+	getWeather = async e => {
+		e.preventDefault();
+
+		const city = e.target.elements.city.value
+		const country = e.target.elements.country.value
+
+		const API_KEY = '0ea76d59a043260036d37b66d82dee6b';
+		const weather_url = 'api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0ea76d59a043260036d37b66d82dee6b';
+		const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+		const data = await api_call.json();
+
+		console.log(data)
+
+		this.setState({
+			city: data.name
+		})
+	}
+
+    render() {
+        return (
+           <div>
+	      	<Menu test={test} />
+	      	<Header />
+	      	<Form getWeather={this.getWeather} />
+	      	<Weather  city={this.state.city}/>
+	      	<Footer />
+	      </div>
+        );
+    }
 }
 
 export default App;
